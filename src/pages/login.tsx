0891da18c6d887f980fbe3461d04331900414773
suyi <945792171@qq.com>
2023-05-25 17:11:34 +0800

    init

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -28,14 +28,14 @@ const Login: React.FC<loginProps> = () => {
         return () => ctx.revert();
     }, []);
     return (
-        <main className=" relative  flex  h-auto w-full flex-col-reverse  bg-[#000002]   lg:h-screen  lg:flex-row ">
+        <main className=" relative  flex  h-auto min-h-screen w-full  flex-col-reverse   bg-[#000002]  lg:flex-row ">
             <NextSeo title="login" />
             {/* Right */}
             <section
                 title="login-right-box"
-                className="flex h-[140vh] flex-1   flex-col  px-12 py-4 md:h-auto"
+                className="flex h-auto   flex-1  flex-col px-12 py-4"
             >
-                <nav className="flex  w-full  flex-1 flex-col items-center justify-between md:flex-row">
+                <nav className="flex  w-full  flex-1 flex-col items-center justify-between lg:flex-row">
                     <div className=" hover:scale-70  z-[40] cursor-pointer  text-2xl font-bold uppercase text-white transition-all duration-300  ">
                         <Image
                             src="/images/logo2.png"
@@ -54,16 +54,12 @@ const Login: React.FC<loginProps> = () => {
                     <div className="bg-gradient-to-r  from-primary to-secondary  bg-clip-text font-Oswald text-4xl font-bold text-transparent shadow-secondary drop-shadow-lg xl:text-6xl">
                         LOG IN
                     </div>
-                    <div className="flex  h-auto flex-col space-y-12 lg:w-full">
+                    <div className="flex   flex-1 flex-col justify-center space-y-16 lg:w-full ">
                         {/* TODO: google login button  */}
                         <SingleLoginButton provider="google" />
-                        <div className=" flex w-full justify-center font-Oswald text-4xl font-bold ">
-                            OR
-                        </div>
+
                         <SingleLoginButton provider="discord" />
-                        <div className=" flex w-full justify-center font-Oswald text-4xl font-bold text-secondary">
-                            OR
-                        </div>
+
                         <SingleLoginButton provider="guest" />
                     </div>
                 </div>
@@ -71,10 +67,10 @@ const Login: React.FC<loginProps> = () => {
             {/* Left */}
             <section
                 title="login-left-box"
-                className="relative grid  w-full flex-1 grid-rows-4 shadow-lg shadow-secondary lg:h-full lg:grid-cols-4 lg:grid-rows-1"
+                className="relative  flex flex-1 flex-col lg:flex-row"
             >
                 <div className="login-bg-warper absolute left-0 top-0 z-50 h-full w-full bg-secondary/20"></div>
-                <div className="login-image  row-span-1 lg:col-span-1 ">
+                <div className="login-image  flex-[1] ">
                     <Image
                         priority={true}
                         src="/images/login/img1.webp"
@@ -84,7 +80,7 @@ const Login: React.FC<loginProps> = () => {
                         height={500}
                     />
                 </div>
-                <div className="login-image row-span-2 lg:col-span-2">
+                <div className="login-image flex-[2]">
                     <Image
                         priority={true}
                         src="/images/login/img2.webp"
@@ -94,7 +90,7 @@ const Login: React.FC<loginProps> = () => {
                         height={500}
                     />
                 </div>
-                <div className="login-image row-span-1 lg:col-span-1">
+                <div className="login-image flex-[1]">
                     <Image
                         priority={true}
                         src="/images/login/img3.webp"
@@ -119,7 +115,7 @@ const SingleLoginButton = ({
     const isMobile = useMediaQuery({ query: "(max-width: 640px)" });
     return (
         <div
-            className="group flex  flex-1 cursor-pointer items-center justify-center space-x-8  rounded-2xl px-24 py-4 text-2xl shadow-lg shadow-secondary md:min-h-[8vh] "
+            className="group flex  h-[8vh] max-h-[8vh] flex-1 cursor-pointer items-center justify-center  space-x-8 rounded-2xl px-24 py-4 text-2xl shadow-lg shadow-secondary  "
             onClick={() => {
                 if (provider === "guest") {
                     router.push("/category");
